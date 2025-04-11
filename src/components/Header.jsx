28ffdd13b78304d181e669bb411cbc558afb9b11
juni <tmdwns0927@naver.com>
2025-04-11 21:56:59 +0900

    Header CSS 수정

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,26 @@
 import React from 'react';
 import Visual from './Visual';
 import Nav from './Nav';
-// import Banners from './Banners';
+import Banners from './Banners';
 
 export default function Header() {
+  const styles = {
+    header: 'w-full overflow-x-hidden',
+    navContainer: 'px-4 md:px-[11.11%] max-w-[1440px] mx-auto',
+    bannerContainer: 'max-w-[1440px] mx-auto',
+  };
+
   return (
-    <header className="w-full overflow-x-hidden">
-      <div className="px-4 md:px-[11.11%] max-w-[1440px]">
+    <header className={styles.header}>
+      <div className={styles.navContainer}>
         <Nav />
       </div>
+
       <Visual />
-      {/* <div className="max-w-[1440px] mx-auto px-4 sm:px-6 lg:px-8">
+
+      <div className={styles.bannerContainer}>
         <Banners />
-      </div> */}
+      </div>
     </header>
   );
 }
